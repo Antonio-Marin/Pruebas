fe6256c189efdd2f8cd9631217ccbf7c4fd6eaa6
Antonio Marín <antoniomarmart3@gmail.com>
2023-09-11 18:25:21 +0200

    fixed group + added character &  stars

--- pjsekai/test.py
+++ pjsekai/test.py
@@ -24,17 +24,55 @@ def main():
     
 def group():
     number = 0
+    r=list()
     print("\n")
     for i in data["groups"]:
-        number+=1
-        print("\t",number,".",i["nameg"])
-    number+=1
-    print("\t",number,". Atrás")
-    number+=1
-    print("\t",number,". Salir\n")
+        number = i["idg"]
+        r.append(number+1)
+        print("\t",number+1,".",i["nameg"])
+    print("\t",number+2,". Atrás")
+    print("\t",number+3,". Salir\n")
+    number+=3
+    var = input("Indica el número de la opción deseada:")
+
+    if var in str(r):
+        character(int(var)-1)
+    elif var == str(number-1):
+        main()
+    elif var == str(number):
+        pass
+    else:
+        print("\nPOR FAVOR INTRODUZCA UN NÚMERO DE LOS QUE SE MUESTRAN")
+        group()
 
 def character(vgroup):
-    pass  
+    print("\n")
+    idis = list()
+    for i in data["groups"][vgroup]["integrants"]:
+        idis.append(i["idi"]+1)
+        print("\t",i["idi"]+1,".",i["namei"])
+    number = len(data["groups"][vgroup]["integrants"]) +1
+    print("\t",number,". Atrás")
+    print("\t",number+1,". salir","\n")
+
+    var = input("Indica el número de la opción deseada:")
+
+    if var in str(idis):
+        totalstar(vgroup,int(var)-1)
+    elif var == str(number):
+        group()
+    elif var == str(number+1):
+        pass
+    else:
+        print("\nPOR FAVOR INTRODUZCA UN NÚMERO DE LOS QUE SE MUESTRAN")
+        character(vgroup)
+
+def totalstar(vgroup,vintegrant):
+    print(data["groups"][vgroup]["integrants"][vintegrant])
+    #TODO: Implementar una "interfaz" mejor que muestre los datos necesarios
+    pass
+
+#TODO: Implementar funcion(es) para implementar añadir estrellas directamente, quitar o modificar los datos (esta última podría pedirtela al ejecutar el código por 1ª vez)
 
 if __name__ == '__main__':
     main()
