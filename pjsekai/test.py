0703c6f227fe1ea08852f0ed552da8e2d2a154ef
Antonio Marín <antoniomarmart3@gmail.com>
2023-09-14 09:37:47 +0200

    "interface" for totalEXP

--- pjsekai/test.py
+++ pjsekai/test.py
@@ -23,7 +23,6 @@ def main():
         main()
     
 def group():
-    number = 0
     r=list()
     print("\n")
     for i in data["groups"]:
@@ -58,7 +57,7 @@ def character(vgroup):
     var = input("Indica el número de la opción deseada:")
 
     if var in str(idis):
-        totalstar(vgroup,int(var)-1)
+        totalexp(vgroup,int(var)-1)
     elif var == str(number):
         group()
     elif var == str(number+1):
@@ -67,12 +66,33 @@ def character(vgroup):
         print("\nPOR FAVOR INTRODUZCA UN NÚMERO DE LOS QUE SE MUESTRAN")
         character(vgroup)
 
-def totalstar(vgroup,vintegrant):
-    print(data["groups"][vgroup]["integrants"][vintegrant])
-    #TODO: Implementar una "interfaz" mejor que muestre los datos necesarios
-    pass
+def totalexp(vgroup,vintegrant):
 
-#TODO: Implementar funcion(es) para implementar añadir estrellas directamente, quitar o modificar los datos (esta última podría pedirtela al ejecutar el código por 1ª vez)
+    #TODO: Implementar una "interfaz" mejor que muestre los datos necesarios
+    print("\n \t",data["groups"][vgroup]["integrants"][vintegrant]["namei"],"\n")
+    print("\t \t","Rango:",data["groups"][vgroup]["integrants"][vintegrant]["rank"])
+    print("\t \t", "Experiencia:",data["groups"][vgroup]["integrants"][vintegrant]["exp"],"/", data["groups"][vgroup]["integrants"][vintegrant]["maxexp"])
+    print("\n\tElige una opción:")
+    print("\n\t1. Añadir Experiencia")
+    print("\t2. Modificar datos")
+    print("\t3. Atrás")
+    print("\t4. Salir\n")
+
+    var = input("Indica el número de la opción deseada:")
+
+    if var == "1":
+        pass
+    elif var == "2":
+        pass
+    elif var == "3":
+        character(vgroup)
+    elif var == "4":
+        pass
+    else:
+        print("\nPOR FAVOR INTRODUZCA UN NÚMERO DE LOS QUE SE MUESTRAN")
+        totalexp(vgroup, vintegrant)
+
+#TODO: Implementar funcion(es) para implementar añadir Experiencia directamente, quitar o modificar los datos (esta última podría pedirtela al ejecutar el código por 1ª vez)
 
 if __name__ == '__main__':
     main()
